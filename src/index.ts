a3ffe2e6a162ca9033bfacd0b9ad4d9760f13d58
Ubuntu <ubuntu@ip-172-31-89-242.ec2.internal>
2021-12-22 19:08:04 +0000

    done

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,72 @@
 /*
-* This program ...
+* This program test stack class.
 *
 * @author  Felipe Garcia Affonso
 * @version 1.0
-* @since   2021-12-04
+* @since   2021-12-09
 */
 
-import * as readline from 'readline';
+class MrCoxallStack {
+  // this is a stack class.
+  private stack: unknown[] = []
 
-function (s: someString) {
-    // Some text
-    return "some text" + someString;
+  push (pushValue: unknown) {
+    // this method adds a value to the stack.
+    this.stack.push(pushValue)
+  }
+
+  showStack () {
+    // this prints out the stack.
+    for (let counter = this.stack.length - 1; counter >= 0; counter--) {
+      console.log(this.stack[counter])
+    }
+  }
+
+  pop () {
+    // this method removes the last part of the stack.
+    let returnValue: unknown
+    if (this.stack.length !== 0) {
+      returnValue = this.stack.pop()
+    } else {
+      returnValue = null
+    }
+    return returnValue
+  }
+
+  peak () {
+    // this method returns the top part of the stack.
+    let returnValue: unknown
+    if (this.stack.length !== 0) {
+      returnValue = this.stack[0]
+    } else {
+      returnValue = null
+    }
+    return returnValue
+  }
+
+  clear () {
+    // this method clears the stack.
+    this.stack.length = 0
+  }
 }
 
-console.log("Some question");
+const aStack = new MrCoxallStack()
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+aStack.push('bob')
+aStack.push(12)
 
-rl.question("", function(answer) {
-  console.log("Thank you for your valuable feedback:", answer);
-  rl.close();
-});
+console.log('Inicial Stack:')
+aStack.showStack()
+
+console.log('Peak Stack:')
+const peakVal: unknown = aStack.peak()
+console.log(peakVal)
+
+console.log('Show Stack:')
+aStack.showStack()
+
+console.log('Clear Stack:')
+aStack.clear()
+aStack.showStack()
+
+console.log('\nDone.')
